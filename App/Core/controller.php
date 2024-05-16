99b6831b827fa711f8e8513ded40d378932cad20
rafael07-dev <deralato817@gmail.com>
2024-05-16 09:18:17 -0500

    Limpiando comentarios innecesarios

--- App/Core/controller.php
+++ App/Core/controller.php
@@ -3,9 +3,6 @@
         public function render($path, $parametres=[], $layotuh=''){
             ob_start();
             require_once(__DIR__ . '/../Views/'.$path.'.view.php');
-            //$content = ob_get_clean();
-
-            //require_once(__DIR__ . '/../Views/layouts/'.$layout.'.layout.php');
         }
     }
 ?>
